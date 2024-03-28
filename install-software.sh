f52e2134df358f1415c9f5d82a6d8cf3c0cdb6de
joost van der Putten <joostvdp92@gmail.com>
2024-03-28 16:06:04 +0100

    Update install-software.sh

--- install-software.sh
+++ install-software.sh
@@ -1,6 +1,7 @@
-sudo apt install zsh
+sudo apt install zsh curl
 
 # make zsh default shell
 chsh -s $(which zsh)
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" # install ohmyzsh
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k # get powerlevel10k theme
