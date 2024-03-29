eaf2eee1e5ebb3064c082c77e66959fea506b955
joost van der Putten <joostvdp92@gmail.com>
2024-03-29 09:43:02 +0100

    Update install-software.sh

--- install-software.sh
+++ install-software.sh
@@ -1,4 +1,4 @@
-sudo apt install zsh curl
+sudo apt install zsh curl kitty &&
 
 # make zsh default shell
 chsh -s $(which zsh)
@@ -6,12 +6,22 @@ chsh -s $(which zsh)
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" # install ohmyzsh
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k # get powerlevel10k theme
 
+# kitty theme
+THEME=https://raw.githubusercontent.com/dexpota/kitty-themes/master/themes/OneDark.conf
+wget "$THEME" -P ~/.config/kitty/kitty-themes/themes
+cd ~/.config/kitty
+ln -s ./kitty-themes/themes/OneDark.conf ~/.config/kitty/theme.conf
+echo "include ./theme.conf" > kitty.conf
+sed -i '6s/.*/    "background": #222222"' ~/.config  kitty/kitty-themes/themes/OneDark.conf
+
+echo "font_family FiraCode Nerd Font Mono" > font.conf
+
 
 curl -sS https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | bash # install zoxide
 
 # install nerd font SourceCodePro
-wget -P ~/.local/share/fonts https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/SourceCodePro.zip \
+wget -P ~/.local/share/fonts https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/FiraCode.zip\
 && cd ~/.local/share/fonts \
-&& unzip SourceCodePro.zip \
-&& rm SourceCodePro.zip \
+&& unzip FiraCode.zip \
+&& rm FiraCode.zip \
 && fc-cache -fv
