be60b8b594ba1494f21f47504f1dcf0622ff1f17
joost van der Putten <joostvdp92@gmail.com>
2024-04-04 15:18:53 +0200

    Update install-software.sh

--- install-software.sh
+++ install-software.sh
@@ -5,6 +5,7 @@ chsh -s $(which zsh)
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" # install ohmyzsh
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k # get powerlevel10k theme
+echo "source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc # add zsh syntax highlighting
 
 # kitty theme
 THEME=https://raw.githubusercontent.com/dexpota/kitty-themes/master/themes/OneDark.conf
